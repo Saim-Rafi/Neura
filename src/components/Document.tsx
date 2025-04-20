a9d9c34d2f0dcbfe1721554716e5cde2ad0f9984
Saim Rafi <143498300+Saim-Rafi@users.noreply.github.com>
2025-04-20 15:54:41 +0530

    added the Avatars

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -10,6 +10,7 @@ import useOwner from "@/lib/useOwner";
 import DeleteDocument from "./DeleteDocument";
 import InviteUser from "./InviteUser";
 import ManageUsers from "./ManageUsers";
+import Avatars from "./Avatars";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -59,6 +60,7 @@ function Document({ id }: { id: string }) {
         <ManageUsers />
 
         {/* Avatars */}
+        <Avatars />
       </div>
 
       <hr className="pb-10" />
